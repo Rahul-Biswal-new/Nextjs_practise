bbefb9d2063689537da063793bbeddbd84261071
Rahul-Biswal-new <rahulbiswalnew@gmail.com>
2023-08-10 20:06:27 +0530

    page

---
+++ dave-grey/app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link';
+
+function About() {
+  return (
+    < >
+    <main className='className="flex min-h-screen flex-col items-center justify-between p-24"'>
+
+        <h1>About</h1>
+        <Link href="/">Link to home Page</Link> 
+    </main>
+    </>
+  )
+}
+
+export default About
